11fef99187e7d305056513ea86630b9f4c0d3a39
Squirrelcoding <rosilescarlos28@gmail.com>
2026-04-15 05:35:21 -0500

    files in central directory can be read

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct zip_header
+typedef struct
 {
 	uint16_t version_needed_to_extract;
 	uint16_t general_purpose_bit_flag;
@@ -18,9 +18,9 @@ struct zip_header
 	uint16_t extra_field_length;
 	size_t file_name;
 	size_t extra_field;
-};
+} zip_header;
 
-struct central_directory_structure
+typedef struct
 {
 	uint16_t version_made_by;
 	uint16_t version_needed_to_extract;
@@ -38,10 +38,10 @@ struct central_directory_structure
 	uint16_t internal_file_attributes;
 	uint32_t external_file_attributes;
 	uint32_t relative_offset_of_local_header;
-	size_t file_name;
-	size_t extra_field;
-	size_t file_comment;
-};
+	char *file_name;
+	char *extra_field;
+	char *file_comment;
+} cd_file_header;
 
 typedef struct
 {
@@ -54,6 +54,24 @@ typedef struct
 	uint16_t zip_file_comment_length;
 	char *zip_file_comment;
 } eocd_record;
+
+char *read_string_u8(uint8_t *source, size_t offset, size_t length)
+{
+	char *ptr = malloc(sizeof(char) * length);
+
+	memcpy(ptr, source + offset, sizeof(char) * length);
+
+	return ptr;
+}
+
+char *read_string_char(char *source, size_t offset, size_t length)
+{
+	char *ptr = malloc(sizeof(char) * length);
+
+	memcpy(ptr, source + offset, sizeof(char) * length);
+
+	return ptr;
+}
 
 int main(void)
 {
@@ -148,46 +166,92 @@ int main(void)
 											 (eocd_buffer[8]);
 
 			eocd.size_of_central_directory_offset = (eocd_buffer[15] << 24) |
-											 (eocd_buffer[14] << 16) |
-											 (eocd_buffer[13] << 8) |
-											 (eocd_buffer[12]);
+													(eocd_buffer[14] << 16) |
+													(eocd_buffer[13] << 8) |
+													(eocd_buffer[12]);
 
 			eocd.zip_file_comment_length = eocd_buffer[16] | eocd_buffer[17] << 8;
 
-			eocd_zip_file_comment_buffer = malloc(sizeof(char) * eocd.zip_file_comment_length);
-
-			// Read the rest of the comment into the buffer
-			memcpy(eocd_zip_file_comment_buffer, eocd_buffer + 18, sizeof(char) * eocd.zip_file_comment_length);
+			eocd.zip_file_comment = read_string_u8(eocd_buffer, 18, eocd.zip_file_comment_length);
 			break;
 		}
 	}
 
-	printf("===== EOCD STRUCT =====\n");
-	//       end of central dir signature    4 bytes  (0x06054b50)
-    //   number of this disk             2 bytes
-	printf("number of this disk %hu\n", eocd.number_of_this_disk);
-    //   number of the disk with the
-    //   start of the central directory  2 bytes
-	printf("number of the disk with the start of the central directory %hu\n", eocd.number_of_disk_with_start_of_central_directory);
-    //   total number of entries in the
-    //   central directory on this disk  2 bytes
-	printf("total number of entries in the central directory on this disk %hu\n", eocd.number_of_entries_in_central_directory_on_disk);
-    //   total number of entries in
-    //   the central directory           2 bytes
-	printf("total number of entries in the central directory %hu\n", eocd.total_entries_in_central_directory);
-    //   size of the central directory   4 bytes
-	printf("size of the central directory %u\n", eocd.size_of_central_directory);
-    //   offset of start of central
-    //   directory with respect to
-    //   the starting disk number        4 bytes
-	printf("offset of start of central directory with respect to the starting disk number %u\n", eocd.size_of_central_directory_offset);
-    //   .ZIP file comment length        2 bytes
-	printf(".ZIP file comment length %hu\n", eocd.zip_file_comment_length);
-    //   .ZIP file comment       (variable size)
+	// Array to store central directory file headers
+	cd_file_header *cd_file_headers = (cd_file_header *)malloc(sizeof(cd_file_header) * eocd.number_of_entries_in_central_directory_on_disk);
+	cd_file_header temp_cd_file_header;
 
-	// Read central directory record into a struct
+	size_t start_of_header = eocd.size_of_central_directory_offset;
+
+	// Loop to keep finding local file headers
+	for (size_t i = 0; i < eocd.number_of_entries_in_central_directory_on_disk; i++)
+	{
+		// Index for where the data actually starts; we add 4 to skip the magic number.
+		size_t idx = start_of_header + 4;
+
+		temp_cd_file_header.version_made_by = buffer[idx] | buffer[idx + 1] << 8;
+		temp_cd_file_header.version_needed_to_extract = buffer[idx + 2] | buffer[idx + 3] << 8;
+		temp_cd_file_header.general_purpose_bit_flag = buffer[idx + 4] | buffer[idx + 5] << 8;
+		temp_cd_file_header.compression_method = buffer[idx + 6] | buffer[idx + 7] << 8;
+		temp_cd_file_header.last_mod_file_time = buffer[idx + 8] | buffer[idx + 9] << 8;
+		temp_cd_file_header.last_mod_file_date = buffer[idx + 10] | buffer[idx + 11] << 8;
+
+		temp_cd_file_header.crc_32 = buffer[idx + 12] |
+									 buffer[idx + 13] << 8 |
+									 buffer[idx + 14] << 16 |
+									 buffer[idx + 15] << 24;
+
+		temp_cd_file_header.compressed_size = buffer[idx + 16] |
+											  buffer[idx + 17] << 8 |
+											  buffer[idx + 18] << 16 |
+											  buffer[idx + 19] << 24;
+
+		temp_cd_file_header.uncompressed_size = buffer[idx + 20] |
+												buffer[idx + 21] << 8 |
+												buffer[idx + 22] << 16 |
+												buffer[idx + 23] << 24;
+
+		temp_cd_file_header.file_name_length = buffer[idx + 24] | buffer[idx + 25] << 8;
+		temp_cd_file_header.extra_field_length = buffer[idx + 26] | buffer[idx + 27] << 8;
+		temp_cd_file_header.file_comment_length = buffer[idx + 28] | buffer[idx + 29] << 8;
+		temp_cd_file_header.disk_number_start = buffer[idx + 30] | buffer[idx + 31] << 8;
+		temp_cd_file_header.internal_file_attributes = buffer[idx + 32] | buffer[idx + 33] << 8;
+
+		temp_cd_file_header.external_file_attributes = buffer[idx + 34] |
+													   buffer[idx + 35] << 8 |
+													   buffer[idx + 36] << 16 |
+													   buffer[idx + 37] << 24;
+
+		temp_cd_file_header.relative_offset_of_local_header = buffer[idx + 38] |
+															  buffer[idx + 39] << 8 |
+															  buffer[idx + 40] << 16 |
+															  buffer[idx + 41] << 24;
+
+		// Read file name
+		temp_cd_file_header.file_name = read_string_char(buffer, idx + 42, temp_cd_file_header.file_name_length);
+		// Read extra field
+		temp_cd_file_header.extra_field = read_string_char(buffer, idx + 42 + temp_cd_file_header.file_name_length, temp_cd_file_header.extra_field_length);
+		// Read file comment
+		temp_cd_file_header.file_comment = read_string_char(
+			buffer, idx + 42 + temp_cd_file_header.file_name_length + temp_cd_file_header.extra_field_length,
+			temp_cd_file_header.file_comment_length);
+
+		// Move this pointer to the start of the next file header
+		start_of_header += 46 + temp_cd_file_header.file_name_length + temp_cd_file_header.extra_field_length + temp_cd_file_header.file_comment_length;
+
+		// Add the current file header to the list of file headers
+		cd_file_headers[i] = temp_cd_file_header;
+	}
+
+	
+
+	free(temp_cd_file_header.file_name);
+	free(temp_cd_file_header.extra_field);
+	free(temp_cd_file_header.file_comment);
 
 	fclose(fp);
 	free(buffer);
+	free(eocd.zip_file_comment);
 	free(eocd_buffer);
+	free(cd_file_headers);
 }
