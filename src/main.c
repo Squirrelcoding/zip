466967d6a82e00b048f9a7848c915a5b768c00d3
Squirrelcoding <rosilescarlos28@gmail.com>
2026-04-14 22:58:01 -0500

    read EOCD

--- src/main.c
+++ src/main.c
@@ -49,12 +49,11 @@ typedef struct
 	uint16_t number_of_disk_with_start_of_central_directory;
 	uint16_t number_of_entries_in_central_directory_on_disk;
 	uint16_t total_entries_in_central_directory;
-	uint16_t size_of_central_directory;
+	uint32_t size_of_central_directory;
 	uint32_t size_of_central_directory_offset;
 	uint16_t zip_file_comment_length;
 	char *zip_file_comment;
 } eocd_record;
-
 
 int main(void)
 {
@@ -63,7 +62,7 @@ int main(void)
 	unsigned char c;
 	size_t file_size;
 
-	fp = fopen("example.zip", "rb");
+	fp = fopen("out.zip", "rb");
 
 	// If the file doesn't exist
 	if (fp == NULL)
@@ -113,6 +112,8 @@ int main(void)
 	}
 
 	eocd_record eocd;
+	uint8_t *eocd_buffer;
+	char *eocd_zip_file_comment_buffer;
 
 	// Find EOCD signature
 	for (size_t offset = file_size - sizeof(char) * 4; offset > 0; offset--)
@@ -122,10 +123,9 @@ int main(void)
 
 		memcpy(signature, buffer + offset, sizeof(char) * 4);
 
-		// Account for endianness
+		// Data is stored in little-endian format
 		if (signature[0] == 0x50 && signature[1] == 0x4b && signature[2] == 0x05 && signature[3] == 0x06)
 		{
-			char *eocd_buffer;
 
 			// Copy the rest of the file into the buffer.
 			size_t remaining_bytes = file_size - offset - 5;
@@ -133,15 +133,61 @@ int main(void)
 			eocd_buffer = malloc(sizeof(char) * remaining_bytes);
 			memcpy(eocd_buffer, buffer + offset + 4, sizeof(char) * remaining_bytes);
 
-			// TODO: Read the EOCD into a struct
+			// First 2 bytes
+			eocd.number_of_this_disk = eocd_buffer[0] | eocd_buffer[1] << 8;
 
-			free(eocd_buffer);
+			eocd.number_of_disk_with_start_of_central_directory = eocd_buffer[2] | eocd_buffer[3] << 8;
+
+			eocd.number_of_entries_in_central_directory_on_disk = eocd_buffer[4] | eocd_buffer[5] << 8;
+
+			eocd.total_entries_in_central_directory = eocd_buffer[6] | eocd_buffer[7] << 8;
+
+			eocd.size_of_central_directory = (eocd_buffer[11] << 24) |
+											 (eocd_buffer[10] << 16) |
+											 (eocd_buffer[9] << 8) |
+											 (eocd_buffer[8]);
+
+			eocd.size_of_central_directory_offset = (eocd_buffer[15] << 24) |
+											 (eocd_buffer[14] << 16) |
+											 (eocd_buffer[13] << 8) |
+											 (eocd_buffer[12]);
+
+			eocd.zip_file_comment_length = eocd_buffer[16] | eocd_buffer[17] << 8;
+
+			eocd_zip_file_comment_buffer = malloc(sizeof(char) * eocd.zip_file_comment_length);
+
+			// Read the rest of the comment into the buffer
+			memcpy(eocd_zip_file_comment_buffer, eocd_buffer + 18, sizeof(char) * eocd.zip_file_comment_length);
 			break;
 		}
 	}
+
+	printf("===== EOCD STRUCT =====\n");
+	//       end of central dir signature    4 bytes  (0x06054b50)
+    //   number of this disk             2 bytes
+	printf("number of this disk %hu\n", eocd.number_of_this_disk);
+    //   number of the disk with the
+    //   start of the central directory  2 bytes
+	printf("number of the disk with the start of the central directory %hu\n", eocd.number_of_disk_with_start_of_central_directory);
+    //   total number of entries in the
+    //   central directory on this disk  2 bytes
+	printf("total number of entries in the central directory on this disk %hu\n", eocd.number_of_entries_in_central_directory_on_disk);
+    //   total number of entries in
+    //   the central directory           2 bytes
+	printf("total number of entries in the central directory %hu\n", eocd.total_entries_in_central_directory);
+    //   size of the central directory   4 bytes
+	printf("size of the central directory %u\n", eocd.size_of_central_directory);
+    //   offset of start of central
+    //   directory with respect to
+    //   the starting disk number        4 bytes
+	printf("offset of start of central directory with respect to the starting disk number %u\n", eocd.size_of_central_directory_offset);
+    //   .ZIP file comment length        2 bytes
+	printf(".ZIP file comment length %hu\n", eocd.zip_file_comment_length);
+    //   .ZIP file comment       (variable size)
 
 	// Read central directory record into a struct
 
 	fclose(fp);
 	free(buffer);
+	free(eocd_buffer);
 }
